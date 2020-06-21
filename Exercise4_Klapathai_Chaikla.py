efa45b6fe9c9f4444338fa41cf9c2a31a70cb324
klapathai <klapathai@gmail.com>
2020-06-21 20:35:06 +0700

    Update Exercise4_Klapathai_Chaikla.py

--- Exercise4_Klapathai_Chaikla.py
+++ Exercise4_Klapathai_Chaikla.py
@@ -8,8 +8,8 @@ eng = 6.5
 busi = 8
 introCom = 10
 com = 9.5
-print("---Your Score---")
-print("Foundation English : ",eng)
-print("General Business : ",busi)
-print("Introduction to Computer Systems : ",introCom)
-print("Computer Programming : ",com)
+print("          ---Your Score---")
+print("Foundation English                : ",eng)
+print("General Business                  : ",busi)
+print("Introduction to Computer Systems  : ",introCom)
+print("Computer Programming              : ",com)
